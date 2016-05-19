824afc28fef161e077ef0d14ada1eeafb0c3945c
LeoCp <leonardocolodette0@gmail.com>
2016-05-19 11:00:03 -0300

    speed

---
+++ AngularJs/speed/angularSpeed.js
@@ -0,0 +1,10 @@
+
+
+//DIRECTIVES
+angular.module("nomeDoModulo")
+  .directive("nomeDaDiretiva", function () {
+    //Essa função retorna o objeto chamado Derective Definition Object
+    return {
+      //Propriedades da diretiva (define o comportamento da Diretiva)
+    };
+});
